b760ad8cb53168035564f3761ef2317b3570be37
Patricia Arcanjo <patricia.arcanjo@hotmail.com>
2025-12-18 19:04:00 -0300

    chore(k6): remove duplicate files from test/k6 (moved to tests/k6)

--- test/k6/helpers/baseUrl.js
+++
@@ -1,3 +0,0 @@
-export function getBaseUrl() {
-  return __ENV.BASE_URL || 'http://localhost:3000';
-}
--- test/k6/helpers/email.js
+++
@@ -1,5 +0,0 @@
-export function randomEmail() {
-  const t = Date.now();
-  const r = Math.floor(Math.random() * 10000);
-  return `testuser+${t}${r}@example.com`;
-}
